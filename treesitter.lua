f4f4ed3024074d43c53f03138448b4cdaee9fdc6
James Woodward <james@matoch.com>
2022-07-01 23:10:49 -0600

    Add default languages I just want to be there when I use vim

---
+++ treesitter.lua
@@ -0,0 +1,6 @@
+require('nvim-treesitter.configs').setup {
+    ensure_installed={'c','lua','rust','java','bash','cmake','css','dockerfile','go','html','javascript','json','make','markdown', 'perl','php','python','regex','ruby','toml','vim','yaml'},
+    highlight = {
+	    enable = true
+    }
+}
